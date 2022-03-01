1b67229d818691b6201c239bed87c52fec2ae6e0
Ricardo Soares Ribolli <ricardo.ribolli@inspira.com.br>
2022-03-01 16:18:59 -0300

    First commit

---
+++ react-facts/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import logo from '../images/logo192.png'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={logo} alt="logo" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3> 
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
